438e656d57eb3f80b6ae483ad7716f1e244f650e
Sulaim-Saadath <avoor2005@gmail.com>
2026-07-06 18:43:50 +0530

    jump constructs

---
+++ PythonFundamentals/Python_Syntax_and_basic_datatypes/Control_Constructs/Jump_controlconstructs/break.py
@@ -0,0 +1,9 @@
+# break construct
+
+# 1 -> 6
+# break at 3
+for i in range(1, 7):
+    if i == 4:
+        break
+    print(i)
+    
---
+++ PythonFundamentals/Python_Syntax_and_basic_datatypes/Control_Constructs/Jump_controlconstructs/continue.py
@@ -0,0 +1,7 @@
+# continue
+# 1 -> 6
+# skip 4th iteration
+for i in range(1, 7):
+    if i == 4:
+        continue
+    print(i)
