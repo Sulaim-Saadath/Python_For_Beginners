915dbbd36f90ffb335976158cdf541e45484435d
Sulaim-Saadath <avoor2005@gmail.com>
2026-07-13 23:14:22 +0530

    Inbuilt message in List

--- DataStructures_In_Python/Lists/List.py
+++ DataStructures_In_Python/Lists/List.py
@@ -40,3 +40,36 @@ print(f"{numbers}")
 
 # 3) Length of the List
 print(f"{len(numbers)}")
+
+# Traversing inside List
+for num in numbers:
+    print(num)
+    pass
+
+
+# 4) extend() - Inserting multiple elements inside list
+list1 = [1, 2, 3] 
+list2 = [4, 5, 6]
+list1.extend(list2)
+print(f"{list1}")
+
+# 5) sort() - Arranges the element in ascending order
+list3 = [4, 1, 3, 2]
+list3.sort()
+print(f"{list3}")
+# To arrange in descnding just reverse after sorting in ascending order
+list3.reverse()
+print(f"{list3}")
+
+# Taking input inside a list
+integers = (input().split())
+i = 0
+list4 = []
+for n in integers:
+    list4.append(int(n))
+# print(f"{integers}")
+print(f"{list4}")
+
+# Merging Two Lists
+newList = list1 + list2
+print(f"{newList}")
