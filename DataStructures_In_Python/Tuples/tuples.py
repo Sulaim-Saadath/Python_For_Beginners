9cb82b2c5d8f9ab0d94eb18bdd3924fc80d14c02
Sulaim-Saadath <avoor2005@gmail.com>
2026-07-23 17:50:29 +0530

    Tuples Basics

--- DataStructures_In_Python/Tuples/tuples.py
+++ DataStructures_In_Python/Tuples/tuples.py
@@ -1,7 +1,28 @@
-def studentInfo():
-    name = "Sulaim"
-    age = 21
-    gender = "male"
-    return (name, age, gender)
-student = studentInfo()
-print(f"{student}")
+# Creation of tuple
+t = (1, 3, 6, 7, 8)
+print(f"{t}")
+
+t1 = ("Gamana", 5.4, 100, 'A')
+print(f"{t1}")
+print(f"{type(t1)}")
+
+# It is not recomended to store a single value inside a tuple beacause it will behave as a int not as tuple 
+# => To store only single value and treated as tuple it should written like this
+t2 = (3, )
+print(f"{type(t2)}")
+
+# Accessing a single element of a index
+# Indexing starts from the 0
+print(f"{t1[0]}")
+
+# Acessing using negative values (starts from last element of tuple)
+print(f"{t1[-4]}")
+
+# Tuple Slicing
+print(f"t[1:4]= {t[1:4]}")
+print(f"t[:3]= {t[:3]}")
+
+# Tuples are Immuatable - You cannot the change the values Inside Tuple
+t1[0] = "Saadath"
+print(f"{t1}")
+
