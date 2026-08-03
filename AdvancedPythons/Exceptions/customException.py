f9366dd7a4cb65ce0f4cba9a176dcc703ea40225
Sulaim-Saadath <avoor2005@gmail.com>
2026-08-03 16:33:46 +0530

    customException

--- AdvancedPythons/Exceptions/customException.py
+++ AdvancedPythons/Exceptions/customException.py
@@ -1,11 +1,14 @@
 # CustomException
-class InvalidMarksError(Exception): # Extending the exception to Exception class
+class InvalidMarksError(Exception):# Extending the exception to Exception class
+    def __init__(self, message = "Marks should be between 0 to 100"):
+        self.message = message
+        super().__init__(self.message)
     pass
 
 try:
     marks = int(input("Enter the marks: "))
     if marks < 0 or marks > 100:
-        raise InvalidMarksError("Marks should be between 0 to 100")
+        raise InvalidMarksError()
     print(f"Marks is {marks}")
 except InvalidMarksError as e:
-    print("Custom error:",e)
+    print(f"Custom error: {e}")
