b37037f9fc243ff26360ba9572f59bfffa410dd1
Sulaim-Saadath <avoor2005@gmail.com>
2026-08-04 16:36:50 +0530

    Iterators, Generators, Decorators, Decorators with arguments

--- AdvancedPythons/Decorator/decorator.py
+++ AdvancedPythons/Decorator/decorator.py
@@ -33,3 +33,26 @@ def div(a, b):
     print(a/b)
     pass
 div(a,b)
+
+print("---------------Decorater with arguments---------------")
+# Step 1: Define a decorator that takes an argument
+
+def repeat(num_times):
+    def decorator(func):
+        def wrapper():
+            for _ in range(num_times):
+                func()
+        return wrapper
+    return decorator
+
+
+# Step 2: Apply the decorator with an argument
+
+@repeat(num_times=3)
+def greet():
+    print("Hello!")
+
+
+# Step 3: Call the decorated function
+
+greet()
